0e593f6e82559fe8df5afbceb0faedda8c18118d
PythonSupports <158006852+PythonSupports@users.noreply.github.com>
2025-05-01 01:52:43 +0530

    Cᴏᴅᴇʀ~ᴋʀɪsʜ

--- variables.py
+++ variables.py
@@ -24,8 +24,8 @@ class Config(object):
     MONGO_DB_URI = "mongodb+srv://t45:t45@cluster0.plfylpo.mongodb.net/?retryWrites=true&w=majority"
 
     # Support chat and support ID
-    SUPPORT_CHAT = "https://t.me/speeds_net"
-    SUPPORT_ID = - 1002076123692
+    SUPPORT_CHAT = "https://t.me/botLogGroup"
+    SUPPORT_ID = -1002115990090
 
     # Database name
     DB_NAME = "MikoDB"
